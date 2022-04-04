2e722429470dfc1b8b21f62824093d2d6fc328bd
dspic1978 <47073537+dspic1978@users.noreply.github.com>
2022-04-04 20:42:32 +0500

    Update CAtoCert9.sh

--- file/SertRoot/CAtoCert9.sh
+++ file/SertRoot/CAtoCert9.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
- 
+
+apt update
+apt install libnss3-tools
 certfile="russian_trust_root_ca.cer"
 certname="Russian Trusted Root CA"
- 
-for certDB in $(find ~/ -name "cert9.db")
+for certDB in $(find /home -name "cert9.db")
 do
     certdir=$(dirname ${certDB});
     certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
